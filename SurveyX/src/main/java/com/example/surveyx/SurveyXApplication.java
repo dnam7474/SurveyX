3e0c3bda424016ebf44817a78456e4261411e245
dnam7474 <Daniel.Nam@cvent.com>
2025-03-12 11:03:46 -0400

    created springboot project

---
+++ SurveyX/src/main/java/com/example/surveyx/SurveyXApplication.java
@@ -0,0 +1,13 @@
+package com.example.surveyx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurveyXApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SurveyXApplication.class, args);
+    }
+
+}
---
+++ SurveyX/src/test/java/com/example/surveyx/SurveyXApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.surveyx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurveyXApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
